72e23c2184ff82b5e555b38ebb0b884def370ce7
Katharina Kloppenborg <katharina.kloppenborg@gmail.com>
2023-04-19 19:16:05 +0200

    Add test for create_dendrogram function value error for count parameter

--- tests/test_analysis.py
+++ tests/test_analysis.py
@@ -2,8 +2,10 @@ from cardsort import analysis
 import pandas as pd
 import pytest
 
+df = pd.read_csv("tests/test-data.csv")
+
+
 def test_get_distance_matrix():
-    df = pd.read_csv('tests/test-data.csv')
     expected = [2., 1., 5., 5., 5., 4., 5., 5., 5., 1., 5., 5., 
                 5., 5., 5., 5., 4., 5., 5., 5., 5., 5., 5., 5.,
                 2., 1., 4., 3., 3., 5., 3., 2., 1., 1., 5., 3., 
@@ -13,16 +15,22 @@ def test_get_distance_matrix():
 
 def test_get_cluster_labels():
     input = ['Cat', 'Tiger', 'Dog']
-    df = pd.read_csv('tests/test-data.csv')
     expected = ['pets', 'animals', 'Animals']
     actual = analysis.get_cluster_labels(df, input)
     assert actual == expected, "Test data cluster labels incorrectly retreived"
 
 def test_get_cluster_labels_df():
-    input = ['Cat', 'Tiger', 'Dog']
-    df = pd.read_csv('tests/test-data.csv')
-    data_exp = [[1, "pets", ["Dog", "Tiger", "Cat"]],[2, "animals", ["Cat", "Tiger", "Dog"]],[5, "Animals", ["Dog", "Tiger", "Cat"]]] 
+    input = ["Cat", "Tiger", "Dog"]
+    data_exp = [
+        [1, "pets", ["Dog", "Tiger", "Cat"]],
+        [2, "animals", ["Cat", "Tiger", "Dog"]],
+        [5, "Animals", ["Dog", "Tiger", "Cat"]],
+    ]
     expected = pd.DataFrame(data_exp, columns=["user_id", "cluster_label", "cards"])
     expected = expected.astype({'user_id' : 'object'})
     actual = analysis.get_cluster_labels_df(df,input)
     assert actual.equals(expected), "Dataframe does not equal expected dataframe"
+
+def test_create_dendrogram_ve_count():
+    with pytest.raises(ValueError):
+        analysis.create_dendrogram(df, count="I am no count type!") 
